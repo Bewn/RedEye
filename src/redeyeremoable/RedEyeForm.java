975a568c7b585b8f5f3a4b77d4d304d3158ba834
roseberyb <roseberyb@fss-inlab0c8219.ldsb.ads>
2014-11-19 08:58:35 -0500

    Made random labels appear

--- src/redeyeremoable/RedEyeForm.java
+++ src/redeyeremoable/RedEyeForm.java
@@ -1,12 +1,55 @@
 
 package redeyeremoable;
 
+import java.awt.Color;
+import java.util.Random;
+import javax.swing.JLabel;
+import javax.swing.border.LineBorder;
+
 
 public class RedEyeForm extends javax.swing.JFrame {
 
+    
+    final int LW = 8;
+    JLabel [][] grid = new JLabel[LW][LW];
+    JLabel [][] temp = new JLabel[LW][LW];
+    
+    int [][] red = new int[LW][LW];
+    int [][] green = new int[LW][LW];
+    int [][] blue = new int[LW][LW];
+    
+    Color rgb;
+    Random gen = new Random();
    
     public RedEyeForm() {
         initComponents();
+        
+        for(int r = 0; r < LW; r++){
+            
+            for(int c = 0; c < LW; c++){
+                
+                red[r][c] = gen.nextInt(256);
+                green[r][c] = gen.nextInt(256);
+                blue[r][c] = gen.nextInt(256);
+            
+                grid[r][c] = new JLabel();
+                temp[r][c] = new JLabel();
+                grid[r][c].setText("");
+                grid[r][c].setOpaque(true);
+                
+                
+                
+                rgb = new Color(red[r][c],green[r][c],blue[r][c]);
+                grid[r][c].setBackground(rgb);
+                
+                
+                this.getContentPane().add(grid[r][c]);
+                grid[r][c].setBounds(25 + (c*60), 25 + (r*60), 50, 50);
+                grid[r][c].setBorder(new LineBorder(Color.BLACK,1));
+                grid[r][c].setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+            }
+        }
+        
     }
 
     
@@ -14,21 +57,40 @@ public class RedEyeForm extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        remove = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        remove.setText("Change Things");
+        remove.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                removeActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(24, 24, 24)
+                .addComponent(remove)
+                .addContainerGap(480, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(444, Short.MAX_VALUE)
+                .addComponent(remove)
+                .addGap(23, 23, 23))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void removeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_removeActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_removeActionPerformed
 
    
     public static void main(String args[]) {
@@ -63,5 +125,6 @@ public class RedEyeForm extends javax.swing.JFrame {
         });
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton remove;
     // End of variables declaration//GEN-END:variables
 }
